ea6d2e8a3bf9534feb99979efa219daf95d3f6ad
Jakobus <jakob@familie-steiner.net>
2026-04-07 18:28:48 +0200

    created a route structure with basic views

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Hier muss kein Router mehr importiert oder erstellt werden!
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <h1 class="title">Plamo🌱</h1>
+  </div>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink> |
+    <RouterLink to="/about">Go to About</RouterLink> |
+    <RouterLink to="/plants">Go to Plants</RouterLink> |
+    <RouterLink to="/data">Go to Data</RouterLink> |
+    <RouterLink to="/login">Go to Login</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
-
-<style scoped></style>
+<style scoped>
+.title {
+  font-family: Bricolage Grotesque, sans-serif;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  color: white;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background-color: #629584;
+}
+</style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import HomeView from './HomeView.vue'
+import AboutView from './AboutView.vue'
+import LoginView from "@/LoginView.vue";
+import DataView from "@/DataView.vue";
+import PlantsView from "@/PlantsView.vue";
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HomeView },
+    { path: '/about', component: AboutView },
+    { path: '/login', component: LoginView},
+    { path: '/login', component: LoginView},
+    { path: '/data', component: DataView},
+    { path: '/plants', component: PlantsView},
+]
+
+const router = createRouter({
+    // createWebHistory sorgt dafür, dass die URL im Browser mitwandert
+    history: createWebHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
