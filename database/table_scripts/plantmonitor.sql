c605acbc9e3c73d621e2bfec8fb2a380976c99e4
Jakobus <jakob@familie-steiner.net>
2026-04-13 23:48:41 +0200

    update needet deoendencys

--- database/table_scripts/plantmonitor.sql
+++ database/table_scripts/plantmonitor.sql
@@ -2,5 +2,6 @@ CREATE TABLE plantmonitor
 (
     id             serial PRIMARY KEY,
     sensor_id      varchar(256) REFERENCES sensor (name) ON DELETE CASCADE,
-    plant_id      varchar(256) REFERENCES plant (name) ON DELETE CASCADE
+    plant_id      varchar(256) REFERENCES plant (name) ON DELETE CASCADE,
+    broker_id      serial REFERENCES mqqt_broker (id) ON DELETE CASCADE
 )
